674dc9f13661859d62b252f1146e9a44e65a7a05
vikasgupta1204 <vikasgupta1204.vg@gmail.com>
2022-12-19 16:18:55 +0530

    loop through the list in html template and  rendering data based on condition

--- thymeleaf/src/main/java/com/boot/thymeleaf/controller/MyController.java
+++ thymeleaf/src/main/java/com/boot/thymeleaf/controller/MyController.java
@@ -1,20 +1,36 @@
 package com.boot.thymeleaf.controller;
+
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MyController {
-    @RequestMapping(value="/about",method = RequestMethod.GET)
-    public String about(Model model){
+    @RequestMapping(value = "/about", method = RequestMethod.GET)
+    public String about(Model model) {
         System.out.println("About handler");
-        //to send the data from handler to the view
+        // to send the data from handler to the view
         model.addAttribute("name", "vikas");
         model.addAttribute("currentDate", DateFormat.DAY_OF_WEEK_IN_MONTH_FIELD);
         return "about";
+    }
+
+    /*
+     * @RequestMapping(value="/about",method = RequestMethod.GET) and
+     * 
+     * @GetMapping("/about") are same
+     */
+
+    @GetMapping("/example-loop")
+    public String iterateController(Model m) {
+
+        m.addAttribute("list",List.of("Vikas","Raju","Vivek","Gaurav","Shekhar"));
+        return "iterate";
     }
 }
