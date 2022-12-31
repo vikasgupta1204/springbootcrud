6eb554a0e71c97546ac5593de9f395fa5be18f87
vikasgupta1204 <vikasgupta1204.vg@gmail.com>
2023-01-01 04:11:15 +0530

    smartcontact manager configurations

---
+++ contactmanager/src/test/java/com/spring/cm/contactmanager/ContactmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cm.contactmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
