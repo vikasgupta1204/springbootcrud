51ca52f9ae88801f104707a60e67828b32fe0207
vikasgupta1204 <vikasgupta1204.vg@gmail.com>
2023-01-11 09:10:59 +0530

    creating responsive sidebar for contact_manager

--- contactmanager/src/main/java/com/spring/cm/contactmanager/controller/UserController.java
+++ contactmanager/src/main/java/com/spring/cm/contactmanager/controller/UserController.java
@@ -1,14 +1,27 @@
 package com.spring.cm.contactmanager.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.cm.contactmanager.dao.UserRepository;
+import com.spring.cm.contactmanager.entities.User;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
-    
+    @Autowired
+    private UserRepository userRepository;
     @RequestMapping("/index")
-    public String dashboard(){
+    public String dashboard(Model model,Principal principal){
+        String userName=principal.getName();
+        System.out.println("USERNAME: "+userName);
+        User user=userRepository.getUserByUserName(userName);
+        System.out.println("USER ->"+user);
+        model.addAttribute("user", user);
         return "normal/user_dashboard";
     }
 }
--- contactmanager/src/main/resources/static/js/script.js
+++ contactmanager/src/main/resources/static/js/script.js
@@ -1,1 +1,14 @@
 console.log("This is script file")
+
+const toggleSidebar=()=>{
+    if($('.sidebar').is(":visible")){
+        //if sidebar visible then close it
+        $(".sidebar").css("display","none")
+        $(".content").css("margin-left","0%")
+    }
+    else{
+        //if sidebar not visible then show it
+        $(".sidebar").css("display","block")
+        $(".content").css("margin-left","20%")
+    }
+}
