bad4d2c5a6bf2dfd4d89a31b5fc512e0a1a20d60
vikasgupta1204 <vikasgupta1204.vg@gmail.com>
2023-01-27 21:29:16 +0530

    implement pagination

--- contactmanager/src/main/java/com/spring/cm/contactmanager/controller/UserController.java
+++ contactmanager/src/main/java/com/spring/cm/contactmanager/controller/UserController.java
@@ -13,10 +13,14 @@ import javax.servlet.http.HttpSession;
 import org.apache.tomcat.util.http.fileupload.FileUpload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -100,13 +104,16 @@ public class UserController {
     }
 
     //show contacts handler
-    @GetMapping("/show-contacts")
-    public String showContacts(Model model,Principal principal){
+    @GetMapping("/show-contacts/{page}")
+    public String showContacts(@PathVariable("page") Integer page, Model model,Principal principal){
         model.addAttribute("title","Show User Contacts");
         String userName=principal.getName();
         User user=this.userRepository.getUserByUserName(userName);
-        List<Contact> contacts=this.contactRepository.findContactByUser(user.getId());
+        Pageable pageable= PageRequest.of(page, 3);
+        Page<Contact> contacts=this.contactRepository.findContactByUser(user.getId(),pageable);
         model.addAttribute("contacts",contacts);
+        model.addAttribute("currentPage",page);
+        model.addAttribute("totalPages",contacts.getTotalPages());
         /*send contact list to view
         String username=principal.getName();
         User user=this.userRepository.getUserByUserName(username);
--- contactmanager/src/main/java/com/spring/cm/contactmanager/dao/ContactRepository.java
+++ contactmanager/src/main/java/com/spring/cm/contactmanager/dao/ContactRepository.java
@@ -2,6 +2,8 @@ package com.spring.cm.contactmanager.dao;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,6 +13,7 @@ import com.spring.cm.contactmanager.entities.Contact;
 public interface ContactRepository extends JpaRepository<Contact,Integer> {
     //pagination....
     @Query("from Contact as c where c.user.id=:userId")
-    public List<Contact> findContactByUser(@Param("userId") int userId);
+    /*pageable contains two information i.e current page and contact per page */
+    public Page<Contact> findContactByUser(@Param("userId") int userId, Pageable pageable); 
 
 }
