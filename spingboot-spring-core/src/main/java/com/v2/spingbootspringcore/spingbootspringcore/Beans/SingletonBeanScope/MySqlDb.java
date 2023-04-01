dd38b026a1bfe03423126121c03abff1779dd653
vikasgupta1204 <vikasgupta1204.vg@gmail.com>
2023-04-01 16:48:22 +0530

    Prototype Bean Scope

--- spingboot-spring-core/src/main/java/com/v2/spingbootspringcore/spingbootspringcore/Beans/SingletonBeanScope/MySqlDb.java
+++ spingboot-spring-core/src/main/java/com/v2/spingbootspringcore/spingbootspringcore/Beans/SingletonBeanScope/MySqlDb.java
@@ -1,10 +1,13 @@
 package com.v2.spingbootspringcore.spingbootspringcore.Beans.SingletonBeanScope;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class MySqlDb implements Database{
     @Override
     public void connect() {
